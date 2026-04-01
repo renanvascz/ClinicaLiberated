2ae723c2487186aaa7d4855337d97c51db3f90ff
renanvascz <renanvasconcelos116@protonmail.com>
2026-04-01 16:33:33 -0300

    Métodos Get Criados

---
+++ ClinicaLiberated/Controllers/SaudeController.cs
@@ -0,0 +1,45 @@
+﻿using ClinicaLiberated.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicaLiberated.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SaudeController : Controller
+    {
+
+        [HttpGet("retornoCasa")]
+        public string casa()
+        {
+            return "casa";
+        }
+
+        [HttpGet("nomePaciente")]
+
+        public string paciente()
+        {
+            string nome = "Giovanni";
+            return "Paciente: " + nome;
+        }
+        [HttpGet("listaPacientes")]
+        public List<string> listaNome()
+        {
+            List <string> listaPacientes = new List<string>();
+            listaPacientes = ["Giovanni", "Carlos", "Pedro"];
+            return listaPacientes;
+        }
+        [HttpGet("pacientes")]
+        public List<Paciente> ListaPaciente()
+        {
+            Paciente novoPaciente = new Paciente("1019210", "Giovanni", "10/04/1999", "Vermelha");
+            List<Paciente> listaPaciente = new List<Paciente>();
+            listaPaciente.Add(novoPaciente);
+            novoPaciente = new Paciente("1020220", "Eduarda","15/03/1990","Verde");
+            listaPaciente.Add(novoPaciente);
+            return listaPaciente;
+
+        }
+
+    }
+}
---
+++ ClinicaLiberated/Models/Paciente.cs
@@ -0,0 +1,21 @@
+﻿namespace ClinicaLiberated.Models
+{
+    public class Paciente
+    {
+        public string? nomeCompleto {  get; set; }
+        public string? email { get; set; }
+        public string? endereco { get; set; }
+        public string? prioridade { get; set; }
+        public string? cpf {  get; set; }
+        public string? telefone { get; set; }
+        public string? dataNascimento {  get; set; }
+
+        public Paciente(string? cpf, string? nomecompleto, string? prioridade, string? dataNascimento)
+        {
+            this.cpf = cpf;
+            this.nomeCompleto = nomecompleto;
+            this.dataNascimento = dataNascimento;
+            this.prioridade = prioridade;
+        }
+    }
+}
