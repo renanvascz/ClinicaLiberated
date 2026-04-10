41233fe27233c19413d9a00ee892bad80d3a0711
renanvascz <renanvasconcelos116@protonmail.com>
2026-04-10 16:20:40 -0300

    Atualização do Codigo 4

--- ClinicaLiberated/Controllers/MedicoController.cs
+++ ClinicaLiberated/Controllers/MedicoController.cs
@@ -11,6 +11,8 @@ namespace ClinicaLiberated.Controllers
     {
 
         public static List<MedicoModel> listaMedicos = new List<MedicoModel>();
+        private IEnumerable<object> listaMedico;
+
         [HttpPost("cadastroMedico")]
         public string cadastroMedico([FromBody] MedicoModel medico)
         {
@@ -26,10 +28,58 @@ namespace ClinicaLiberated.Controllers
         }
 
         //editar médico
+        //public List<MedicoModel>
+
+        [HttpPut("editarMedico/{id}")]
+        public string editarMedico([FromBody] MedicoModel medicoEditado, string id)
+        {
+            foreach (var medico in listaMedicos)
+            {
+                if (medico.crm == id)
+                {
+                    medico.crm = medicoEditado.crm;
+                    medico.nomeCompleto = medicoEditado.nomeCompleto;
+                    medico.telefone = medicoEditado.telefone;
+                    medico.email = medicoEditado.email;
+                    medico.dataNascimento = medicoEditado.dataNascimento;
+                    return $"Medico {medico.nomeCompleto}, crm anterior: {id} editado com sucesso";
+
+                }
+            }
+            return "Paciente não encontrado.";
+        }
 
 
         //buscar médico
 
+        [HttpGet("buscarMedico/{id}")]
+        public MedicoModel? buscarMedico(string id)
+        {
+            foreach (var medico in listaMedicos)
+            {
+                if (medico.crm == id)
+                {
+                    return medico;
+                }
+            }
+            return null;
+        }
+
         //excluir médico
+        [HttpDelete("deletarMedico/{id}")]
+        public string? deletarMedico (string id)
+        {
+            foreach (var medico in listaMedicos)
+            {
+                if (medico.crm == id)
+                {
+                    listaMedicos.Remove(medico);
+                    return $"Medico: {id} deletado com sucesso";
+                }
+            }
+            return "Medico não encontrado";
+        }
+
+
     }
 }
--- ClinicaLiberated/Program.cs
+++ ClinicaLiberated/Program.cs
@@ -12,6 +12,7 @@ namespace ClinicaLiberated
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
@@ -19,6 +20,8 @@ namespace ClinicaLiberated
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
